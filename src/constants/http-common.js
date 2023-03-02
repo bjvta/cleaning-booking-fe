8d9cb054d71bff613bf6e127cde6d47e2f8ff3f2
Brandon Jason <bjvtamayo78@gmail.com>
2023-03-01 23:33:50 -0500

    add the prod url

--- src/constants/http-common.js
+++ src/constants/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://mysite-idgd.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json"
   }
